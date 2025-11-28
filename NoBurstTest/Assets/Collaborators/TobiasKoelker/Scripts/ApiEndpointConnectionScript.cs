ac079acf7570af50f8dabaa94924d876b977a16b
Robzon33 <48408667+Robzon33@users.noreply.github.com>
2025-11-28 14:02:36 +0100

    Update folders

--- NoBurstTest/Assets/Collaborators/TobiasKoelker/Scripts/ApiEndpointConnectionScript.cs
+++ NoBurstTest/Assets/Collaborators/TobiasKoelker/Scripts/ApiEndpointConnectionScript.cs
@@ -5,7 +5,7 @@ public class ApiEndpointConnectionScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int a = 4;
     }
 
     // Update is called once per frame
